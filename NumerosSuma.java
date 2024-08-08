8d3eadaf8fe98e3064b4696c510efc28e3918435
Alejo1628 <alejogrxd@gmail.com>
2024-08-07 23:30:50 -0500

    Net

---
+++ NumerosSuma.java
@@ -0,0 +1,27 @@
+
+
+package com.mycompany.numerossuma;
+
+import java.util.Scanner;
+
+
+public class NumerosSuma {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b,c;
+        
+        a=sc.nextInt();
+        b=sc.nextInt();
+        
+        c = a+b;
+        
+        
+        System.out.println("s");
+        
+        System.out.println("Suma es igual a "+c);
+        
+        
+    }
+}
